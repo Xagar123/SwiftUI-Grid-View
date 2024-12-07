e8720c783eba7736fe5d7e4806e67e04a23bd10f
sagar <sagar@THINHYDL027.local>
2024-12-08 02:15:06 +0530

    dynamic grid view

---
+++ gridView/gridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  gridViewApp.swift
+//  gridView
+//
+//  Created by sagar on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct gridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
